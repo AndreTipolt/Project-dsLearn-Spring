7f0242e5139362229004cb330f3b54d10008e70f
andretipolt <andretipoltlopes@gmail.com>
2023-07-10 10:36:54 -0300

    Create entity Course and seed your table

---
+++ src/main/java/tipolt/andre/dslearn/entities/Course.java
@@ -0,0 +1,27 @@
+package tipolt.andre.dslearn.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "tb_course")
+@Data
+public class Course implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String imgUri;
+
+    private String imgGrayUri;
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,3 +16,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course(name, img_uri, img_gray_uri) VALUES ('BootCamp HTML e CSS', 'https://cdn.pixabay.com/photo/2020/05/31/09/30/online-course-5241968_1280.jpg', 'https://cdn.pixabay.com/photo/2017/03/08/14/20/flat-2126876_1280.png');
